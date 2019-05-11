918034ad8d0f435c16df1b58fe5617ecb5772685
Kononov Zakhar <z.kononov@rambler-co.ru>
2019-05-12 00:12:18 +0300

    remove redundant markdown styles and extra attributes

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.use('/answers', express.static(PATH.join(__dirname, 'answers')))
-app.use('/public', express.static(PATH.join(__dirname, 'styles')))
+app.use('/public', express.static(PATH.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
   res.sendFile(PATH.join(__dirname, 'index.html'))
