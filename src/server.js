d012044c09a56dfad025772bd10be6e02d59d980
Kononov Zakhar <z.kononov@rambler-co.ru>
2019-05-12 01:31:56 +0300

    Finnish restyling

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const PATH = require('path')
+
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.use('/answers', express.static(PATH.join(__dirname, 'answers')))
+app.use('/public', express.static(PATH.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+  res.sendFile(PATH.join(__dirname, 'index.html'))
+})
+
+app.listen(port, () => {
+  console.log('Server is running on ' + port)
+})
